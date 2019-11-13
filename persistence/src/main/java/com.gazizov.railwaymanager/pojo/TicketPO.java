b69cefba33d9c6fc0a439de865f2004f0cdb50d1
Romario <dragorin91@mail.ru>
2019-11-13 21:09:03 +0300

    WIN

--- persistence/src/main/java/com.gazizov.railwaymanager/pojo/TicketPO.java
+++ persistence/src/main/java/com.gazizov.railwaymanager/pojo/TicketPO.java
@@ -2,8 +2,6 @@ package com.gazizov.railwaymanager.pojo;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
--- src/main/java/com/gazizov/railwaymanager/services/PassengerService.java
+++ src/main/java/com/gazizov/railwaymanager/services/PassengerService.java
@@ -1,55 +1,55 @@
-package com.gazizov.railwaymanager.services;
-
-import com.gazizov.railwaymanager.persistence.dao.PassengerDao;
-import com.gazizov.railwaymanager.persistence.daoimpl.PassengerDaoImpl;
-import com.gazizov.railwaymanager.persistence.pojo.PassengerPO;
-import org.springframework.beans.factory.annotation.Autowired;
-
-/**
- * 05.11.2019
- *
- * @author Roman Gazizov
- */
-public class PassengerService {
-
-    @Autowired
-    PassengerDao passengerDao;
-
-    PassengerPO createPassenger(String login, String pwd,
-                                String firstName, String lastName, String birthDate) {
-
-        return null;
-
-    }
-
-    public PassengerPO findPassengerByLoginAndCheckPassword(String login, String password) {
-        PassengerPO passengerPO = passengerDao.findByLogin(login);
-        if (passengerPO != null) {
-            if (password.isEmpty()) {
-                System.out.println("Enter password");
-                return null;
-            } else {
-                if (password.equals(passengerPO.getPassword())) {
-                    return passengerPO;
-                } else {
-                    System.out.println("Incorrect password");
-                }
-            }
-        } else {
-            return null;
-        }
-        return null;
-    }
-
-    void updatePassenger(String login, String pwd) {
-
-    }
-
-    void deletePassenger(String login, String pwd) {
-
-
-    }
-
-
-}
-
+//package com.gazizov.railwaymanager.services;
+//
+//import com.gazizov.railwaymanager.dao.PassengerDao;
+//import com.gazizov.railwaymanager.daoimpl.PassengerDaoImpl;
+//import com.gazizov.railwaymanager.pojo.PassengerPO;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+///**
+// * 05.11.2019
+// *
+// * @author Roman Gazizov
+// */
+//public class PassengerService {
+//
+//    @Autowired
+//    PassengerDao passengerDao;
+//
+//    PassengerPO createPassenger(String login, String pwd,
+//                                String firstName, String lastName, String birthDate) {
+//
+//        return null;
+//
+//    }
+//
+//    public PassengerPO findPassengerByLoginAndCheckPassword(String login, String password) {
+//        PassengerPO passengerPO = passengerDao.findByLogin(login);
+//        if (passengerPO != null) {
+//            if (password.isEmpty()) {
+//                System.out.println("Enter password");
+//                return null;
+//            } else {
+//                if (password.equals(passengerPO.getPassword())) {
+//                    return passengerPO;
+//                } else {
+//                    System.out.println("Incorrect password");
+//                }
+//            }
+//        } else {
+//            return null;
+//        }
+//        return null;
+//    }
+//
+//    void updatePassenger(String login, String pwd) {
+//
+//    }
+//
+//    void deletePassenger(String login, String pwd) {
+//
+//
+//    }
+//
+//
+//}
+//
